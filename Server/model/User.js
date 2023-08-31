0a86e57b52936e511cbe775be67550029a6582f3
Shashank Raj <shashankraj3636@gmail.com>
2023-09-01 05:18:50 +0530

    added accounts

--- Server/model/User.js
+++ Server/model/User.js
@@ -2,20 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-  {
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    cart: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Products",
-      },
-    ],
-  },
-  {
-    timestamps: true,
-  }
-);
+const userSchema = new Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  firstName: { type: String, default: "New" },
+  lastName: { type: String, default: "User" },
+  phoneNo: { type: Number },
+  cart: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Products",
+    },
+  ],
+});
 
 module.exports = mongoose.model("User", userSchema);
